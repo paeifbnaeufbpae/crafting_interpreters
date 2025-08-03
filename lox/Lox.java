e305eb945cc7301a40640a3a096619049aa7489e
Leon Tepe <leontepe@icloud.com>
2025-08-03 12:08:01 +0200

    chapter 6 implemented (recursive descent parser)

--- lox/Lox.java
+++ lox/Lox.java
@@ -46,15 +46,36 @@ public class Lox {
   private static void run(String source) {
     Scanner scanner = new Scanner(source);
     List<Token> tokens = scanner.scanTokens();
+    Parser parser = new Parser(tokens);
+    Expr expression = parser.parse();
 
-    // for now, just print the tokens
-    for (Token token : tokens) {
-      System.out.println(token);
-    }
+    // stop if there was a syntax error
+    if (hadError) return;
+
+    // // for now, just print the tokens
+    // for (Token token : tokens) {
+    //   System.out.println(token);
+    // }
+
+    System.out.println(new AstPrinter().print(expression));
   }
 
-  static void error(int line, String message) {
+  static void error(
+    int line,
+    String message
+  ) {
     report(line, "", message);
+  }
+
+  static void error(
+    Token token,
+    String message
+  ) {
+    if (token.type == TokenType.EOF) {
+      report(token.line, " at end", message);
+    } else {
+      report(token.line, " at '" + token.lexeme + "'", message);
+    }
   }
 
   private static void report(
